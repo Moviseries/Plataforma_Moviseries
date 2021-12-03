ed84f058acdd351a451530bde2a9803a8ceba7eb
Jhon salazar <jhonzallazar@gmail.com>
2021-12-02 20:49:38 -0500

    Se le dio diseño al login y register

--- Front-end/src/components/pages/Login.jsx
+++ Front-end/src/components/pages/Login.jsx
@@ -1,30 +1,47 @@
 import React,{useState} from 'react'
-import { Container, Form, Button, Stack } from 'react-bootstrap';
-import { useForm } from 'react-hook-form';
+import { Container, Form, Button, Card } from 'react-bootstrap';
 
 const Login = () => {
 
-    const {register,handleSubmit, formState: {errors}} = useForm();
-    const [entradas, setentradas] = useState([])
-    const onSubmit = (data, e) => {
-        console.log(data)
-        setentradas([...entradas,data])
-        e.target.reset()
-    }
+    const [validated, setValidated] = useState(false);
 
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+    
+        setValidated(true);
+      };
     return ( 
         <Container>
-            <h1 className="mt-3">Login</h1>
-
-                <Form className="col-md-5 mx-auto" onSubmit={handleSubmit(onSubmit)}>
+            <h1 className="mb-3 mt-3">Register</h1>
+            <Card className="col-md-6 mx-auto" border="dark">
+                <Form className="m-3" noValidate validated={validated} onSubmit={handleSubmit}>
                     <Form.Group>
                         <Form.Label>Nombre de usuario</Form.Label>
-                        <Form.Control type="text"/>
-                        <Form.Label>Correo</Form.Label>
-                        <Form.Control type="password"/>
+                        <Form.Control 
+                            type="text"
+                            required
+                            defaultValue=""
+                        />
+                        <Form.Control.Feedback>¡userName valido!</Form.Control.Feedback>
+                        <Form.Control.Feedback type="invalid">¡Ingresa un username!</Form.Control.Feedback>
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label>Contraseña</Form.Label>
+                        <Form.Control 
+                            type="password"
+                            required
+                            defaultValue=""/>
+                        <Form.Control.Feedback>Contraseña valida</Form.Control.Feedback>
+                        <Form.Control.Feedback type="invalid">¡Necesitas una contraseña!</Form.Control.Feedback>
+                        <Button type="submit" variant="dark" className="m-3">Registrar</Button>
                     </Form.Group>
                 </Form>
-
+            </Card>
         </Container>
     );
 }
--- Script_DB/Inser_Genders.sql
+++ Script_DB/Inser_Genders.sql
@@ -4,14 +4,16 @@ LIMIT 0, 1000
 
 -- Date: 2021-12-02 19:40
 */
-INSERT INTO `` (`id_gender`,`name_gender`) VALUES (1,'Accion');
-INSERT INTO `` (`id_gender`,`name_gender`) VALUES (3,'Ciencia Ficción');
-INSERT INTO `` (`id_gender`,`name_gender`) VALUES (4,'Comedia');
-INSERT INTO `` (`id_gender`,`name_gender`) VALUES (5,'Romance');
-INSERT INTO `` (`id_gender`,`name_gender`) VALUES (6,'Drama');
-INSERT INTO `` (`id_gender`,`name_gender`) VALUES (7,'Aventura');
-INSERT INTO `` (`id_gender`,`name_gender`) VALUES (8,'Terror');
-INSERT INTO `` (`id_gender`,`name_gender`) VALUES (9,'Fantasia');
-INSERT INTO `` (`id_gender`,`name_gender`) VALUES (10,'Animación');
-INSERT INTO `` (`id_gender`,`name_gender`) VALUES (11,'Música');
-INSERT INTO `` (`id_gender`,`name_gender`) VALUES (12,'Suspenso');
+use movieserie;
+
+INSERT INTO `gender` (`id_gender`,`name_gender`) VALUES (1,'Accion');
+INSERT INTO `gender` (`id_gender`,`name_gender`) VALUES (3,'Ciencia Ficción');
+INSERT INTO `gender` (`id_gender`,`name_gender`) VALUES (4,'Comedia');
+INSERT INTO `gender` (`id_gender`,`name_gender`) VALUES (5,'Romance');
+INSERT INTO `gender` (`id_gender`,`name_gender`) VALUES (6,'Drama');
+INSERT INTO `gender` (`id_gender`,`name_gender`) VALUES (7,'Aventura');
+INSERT INTO `gender` (`id_gender`,`name_gender`) VALUES (8,'Terror');
+INSERT INTO `gender` (`id_gender`,`name_gender`) VALUES (9,'Fantasia');
+INSERT INTO `gender` (`id_gender`,`name_gender`) VALUES (10,'Animación');
+INSERT INTO `gender` (`id_gender`,`name_gender`) VALUES (11,'Música');
+INSERT INTO `gender` (`id_gender`,`name_gender`) VALUES (12,'Suspenso');
